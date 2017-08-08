d6f4ae45b8e9c7938302f0d668616cfd9a6a8034
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2017-08-08 14:09:16 -0700

    Move to SASS and final pre-release changes

--- functions.php
+++ functions.php
@@ -1,52 +1,27 @@
 <?php
 /**
- * Enqueue Parent stylesheet
- *
- */
+* Enqueue Parent stylesheet
+*
+*/
 
 add_action( 'wp_enqueue_scripts', 'collegechannel_enqueue_styles' );
 function collegechannel_enqueue_styles() {
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 
 /**
- * Hide default credit text (sorry!)
- */
+* Hide default credit text (sorry!)
+*/
 function collegechannel_credits_text( $text ) {
-	return false;
+    return false;
 }
 
 add_filter( 'tinyframework_credits_text', 'collegechannel_credits_text' );
 
 /**
- * Add logo
- *
- * This should be able to be replaced by default logo system in WP 4.5
- */
-function collegechannel_logo() {
-	$output  = '<div id="collegechannel-logo"><a href="';
-	$output .= get_home_url();
-	$output .= '"><img src="';
-	$output .= get_stylesheet_directory_uri ();
-	$output .= '/img/college-channel-logo-246x75.png" alt="';
-	$output .= get_bloginfo( 'name' );
-	$output .= ' logo with link to home page" ></a></div>';
-	echo $output;
+* Registers an editor stylesheet for the theme.
+*/
+function collegechannel_add_editor_styles() {
+	add_editor_style( 'style.css' );
 }
-add_action( 'tha_header_top', 'collegechannel_logo' );
-
-/**
- * Add search (example from default child theme)
- *
- */
-function collegechannel_add_search_to_wp_menu ( $items, $args ) {
-	if( 'primary' === $args -> theme_location ) {
-	$items .= '<li class="menu-item menu-item-search">' . get_search_form(false) . '</li>';
-	}
-	return $items;
-}
-add_filter( 'wp_nav_menu_items','collegechannel_add_search_to_wp_menu',10,2 );
-
-
-/* Enable shortcodes in widget text */
-add_filter('widget_text', 'do_shortcode');
+add_action( 'admin_init', 'collegechannel_add_editor_styles' );
